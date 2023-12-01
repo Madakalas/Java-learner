5dd7827236ff9b133eefe0ea340d3c4824e7dabb
SIDDU REDDY <116141072+Madakalas@users.noreply.github.com>
2023-12-01 14:49:05 +0530

    Add files via upload

---
+++ one.java
@@ -0,0 +1,12 @@
+package pack1;
+public class one 
+{
+    public void User()
+    {
+
+            System.out.println("pack-1 is working");
+        }
+    }
+
+
+
---
+++ two.java
@@ -0,0 +1,13 @@
+package pack2;
+import pack1.one;
+
+
+public class two {
+    public static void main(String[] args)
+    {
+       two t= new two();
+       t.User();
+       System.out.println("pack-2 is executing");
+    }
+    
+}
