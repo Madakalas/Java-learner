7ef29e797ade08919f95cf23df3634d3ea57b2db
SIDDU REDDY <116141072+Madakalas@users.noreply.github.com>
2023-12-01 14:49:35 +0530

    Add files via upload

---
+++ Example12.java
@@ -0,0 +1,16 @@
+package Packages.Loops.Looping;
+class Looping3
+{
+    public static void main(String[] args)
+    {
+        int x = 10;
+        if(x<=20)
+        {
+            System.out.println(x);
+            break ; //dont use break in conditions
+        }
+        else{
+            System.out.println("loop is not correct");
+        }
+    }
+}
