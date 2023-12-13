a02ffd29eddb35ea08095bec9dd6920b837f4184
SIDDU REDDY <116141072+Madakalas@users.noreply.github.com>
2023-12-13 15:01:02 +0530

    Add files via upload

---
+++ One.java
@@ -0,0 +1,7 @@
+public class One{
+    public static void main(String[] args){
+
+        One  t = new One();
+        System.out.println(t);
+}
+}
--- Two.java
+++ Two.java
@@ -1,29 +1,14 @@
-class Grandpa {
-    void m1()
-    {
-        System.out.println("m1-method in GP");
-    }
-    
-}
-class Parent extends Grandpa
-{
-    void m2()
-    {
-        System.out.println("m2 method in Parent");
-    }
-}
-class Child extends Parent
-{
-    void m3()
-    {
-        System.out.println("m3 method in Child");
-    }
-}
-class Test{
+import java.util.ArrayList;
+/* we need to import the util pacakges */
+public class Two {
     public static void main(String[] args) {
-        Child c = new Child();
-        c.m1();
-        c.m2();
-        c.m3();
+            Two t = new Two();
+              t.ArrayList();
+             System.out.println(t);   
     }
+
+    private void ArrayList() {
+    }  
 }
+
+
